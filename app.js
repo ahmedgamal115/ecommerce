69f2ec84fc079db5d29b82607d1154adc642979f
ahmedgamal115 <ahmedgamal1153gmail.com>
2024-02-29 23:23:19 +0200

    fix error deloy again

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cron = require('node-cron');
 const graphqlUploadExpress = require('graphql-upload/graphqlUploadExpress.js');
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(graphqlUploadExpress());
 app.use('/images', express.static('images'));
